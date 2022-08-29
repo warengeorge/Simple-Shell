fa480c7cdad21083dfb2ebd02d83aeceb3ebc1b4
warengeorge <georgeomiebi@gmail.com>
2022-08-29 08:35:38 -0700

    Create: main shell file

---
+++ shell.c
@@ -0,0 +1,11 @@
+#include "shell.h"
+
+/**
+ *
+ * Return: 0 on success, 1 on error, or error on code.
+ */
+
+int main(int argc __attribute__((unused)), char **argv)
+{
+
+}
---
+++ shell.h
@@ -0,0 +1,9 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#endif
