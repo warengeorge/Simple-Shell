bceb07a42892536f2ce059a400038667898cbb0b
PEDA-ALPHA1 <daep100@gmail.com>
2022-09-06 06:03:57 -0700

    Feat: Prompt the user

---
+++ shell_utils.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+/**
+ * prompt_user - Fuction prototype
+ *
+ *
+ * Description: Asks user for input infinitely.
+ * **/
+void prompt_user()
+{
+	static int first_time = 1;
+	if (first_time)
+	{
+		const char* CLEAR_SCREEN_ANSI = " \e[1;1H\e[2J";
+		write (STDOUT_FILENO, CLEAR_SCREEN_ANSI, 12);
+		first_time = 0;
+	}
+	write(1, "($) ", 4);
+}
