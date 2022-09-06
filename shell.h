9375a46f4df733ece0f96913a5e0862a9a3fa59e
PEDA-ALPHA1 <daep100@gmail.com>
2022-09-06 06:02:10 -0700

    Feat: Header files

--- shell.h
+++ shell.h
@@ -1,9 +1,13 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <string.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+void prompt_user;
+int read_command (char cmd[], char *par[]);
 
 #endif
