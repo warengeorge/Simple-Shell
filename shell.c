6c7680f83885c433c86915f781d3f4a391e32396
PEDA-ALPHA1 <daep100@gmail.com>
2022-09-08 14:51:29 -0700

    Feat: Setting up the shell environment

--- shell.c
+++ shell.c
@@ -1,59 +1,40 @@
 #include "shell.h"
-
 /**
- * interactiveShell - processes all interactive shell commands
- * @genHead: general struct
- * Return: Always 0.
+ * main - runs the shell program
+ *
+ * Return: 0 on success
  */
-int interactiveShell(general_t *genHead)
+int main(void)
 {
-	char **bufferTokens = NULL, *buffer = NULL;
-	char *tmp = NULL;
-	size_t len;
+	char *fullpathbuffer = NULL, *copy = NULL, *buffer = NULL;
+	char *PATH = NULL;
+	char **av;
+	int exitstatus = 0;
 
+	signal(SIGINT, SIG_IGN);
+	PATH = _getenv("PATH");
+	if (PATH == NULL)
+		return (-1);
 	while (1)
 	{
-		genHead->nCommands++;
-		printPrompt("($) ");
-		buffer = getUserInput(buffer, &len, genHead);
-		bufferTokens = parseBuffer(buffer, genHead);
-		findBuiltin(genHead, bufferTokens[0]);
-		if (correctAbsPath(bufferTokens[0]))
-			createFork(bufferTokens, genHead);
-		else
+		av = NULL;
+		prompt();
+		buffer = _read();
+		if (*buffer != '\0')
 		{
-			tmp = findCmd(bufferTokens[0]);
-			if (tmp)
-				bufferTokens[0] = tmp;
-			createFork(bufferTokens, genHead);
+			av = tokenize(buffer);
+			if (av == NULL)
+			{
+				free(buffer);
+				continue;
+			}
+			fullpathbuffer = _fullpathbuffer(av, PATH, copy);
+			if (checkbuiltins(av, buffer, exitstatus) == 1)
+				continue;
+			exitstatus = _forkprocess(av, buffer, fullpathbuffer);
 		}
-	}
-	freeStruct(genHead);
-	return (0);
-}
-
-/**
- * nonInteractiveShell - process user commands in non-interactive mode
- * @buffer: input from the command line
- * @genHead: general struct
- * Return: Always 0.
- */
-int nonInteractiveShell(char *buffer, general_t *genHead)
-{
-	char **bufferTokens;
-	char *tmp;
-
-	genHead->nCommands++;
-	bufferTokens = parseBuffer(buffer, genHead);
-	findBuiltin(genHead, bufferTokens[0]);
-	if (correctAbsPath(bufferTokens[0]))
-		createFork(bufferTokens, genHead);
-	else
-	{
-		tmp = findCmd(bufferTokens[0]);
-		if (tmp)
-			bufferTokens[0] = tmp;
-		createFork(bufferTokens, genHead);
+		else
+			free(buffer);
 	}
 	return (0);
 }
